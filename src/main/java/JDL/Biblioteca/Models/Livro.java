2b8a12f34e16056ca3299ae993b9b1ce7eb0fbb1
Joaquimfl8 <160670206+Joaquimfl8@users.noreply.github.com>
2026-03-23 20:03:58 -0300

    substutuir getters/setters por anotações do lombok

--- src/main/java/JDL/Biblioteca/Models/Livro.java
+++ src/main/java/JDL/Biblioteca/Models/Livro.java
@@ -1,5 +1,8 @@
 package JDL.Biblioteca.Models;
 
+import lombok.Getter;
+import lombok.Setter;
+
 @Getter @Setter
 public class Livro extends DbModel {
 
@@ -33,37 +36,4 @@ public class Livro extends DbModel {
         this.genero = genero;
         this.status = status;
     }
-
-    public String getTitulo() { return titulo; }
-    public void setTitulo(String titulo) { this.titulo = titulo; }
-
-    public String getAutor() { return autor; }
-    public void setAutor(String autor) { this.autor = autor; }
-
-    public String getSinopse() { return sinopse; }
-    public void setSinopse(String sinopse) { this.sinopse = sinopse; }
-
-    public String getCapa() { return capa; }
-    public void setCapa(String capa) { this.capa = capa; }
-
-    public String getEditora() { return editora; }
-    public void setEditora(String editora) { this.editora = editora; }
-
-    public String getEdicao() { return edicao; }
-    public void setEdicao(String edicao) { this.edicao = edicao; }
-
-    public int getAno() { return ano; }
-    public void setAno(int ano) { this.ano = ano; }
-
-    public int getDisponiveis() { return disponiveis; }
-    public void setDisponiveis(int disponiveis) { this.disponiveis = disponiveis; }
-
-    public int getExemplares() { return exemplares; }
-    public void setExemplares(int exemplares) { this.exemplares = exemplares; }
-
-    public String getGenero() { return genero; }
-    public void setGenero(String genero) { this.genero = genero; }
-
-    public String getStatus() { return status; }
-    public void setStatus(String status) { this.status = status; }
 }
