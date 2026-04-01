491b05bf38b1c995700677e73aef66fa66085c25
Joaquimfl8 <160670206+Joaquimfl8@users.noreply.github.com>
2026-04-01 14:53:04 -0300

    Interfaces

---
+++ src/main/java/JDL/Biblioteca/Repositories/UsuarioInterface.java
@@ -0,0 +1,10 @@
+package JDL.Biblioteca.Repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import JDL.Biblioteca.Models.*;
+
+@Repository
+public interface UsuarioInterface extends JpaRepository<Livro, Integer> { 
+
+}
